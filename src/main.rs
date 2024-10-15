50aa04695a05362fb11f322fed001c522541fe81
Mohamed Bennekrouf <mohamed.bennekrouf@vd.ch>
2024-10-15 10:29:41 +0200

    Externalize params in yaml

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,15 @@ mod models;
 
 use crate::generate_excel_from_json::generate_excel_from_json;
 use crate::models::ExcelConfig;
+use std::fs::File;
+use std::io::BufReader;
+use serde_yaml::from_reader;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create the Excel configuration
-    let config = ExcelConfig {
-        json_file_path: "all_projects.json".to_string(),
-        output_excel_file: "all_output.xlsx".to_string(),
-        title_bg_color: 0x565E73,        // Blue background for title
-        title_font_color: 0xFFFFFF,      // White font for title using hex code
-        row_alt_color: 0xADD8E6,         // Light blue background for alternating rows
-        repository_name_key: "application".to_string(),  // JSON key for repository name
-        object_array_key: "debt".to_string(),            // JSON key for debt
-    };
+    // Load the configuration from the YAML file
+    let file = File::open("configuration.yml")?;
+    let reader = BufReader::new(file);
+    let config: ExcelConfig = from_reader(reader)?;
 
     // Generate Excel from the JSON input
     generate_excel_from_json(config)?;
--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,27 @@
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
 pub struct ExcelConfig {
     pub json_file_path: String,
     pub output_excel_file: String,
+    #[serde(deserialize_with = "hex_color_to_u32")]
     pub title_bg_color: u32,
+    #[serde(deserialize_with = "hex_color_to_u32")]
     pub title_font_color: u32,
+    #[serde(deserialize_with = "hex_color_to_u32")]
     pub row_alt_color: u32,
     pub repository_name_key: String,
     pub object_array_key: String,
+}
+
+// Helper function to convert a hex string in YAML to u32
+fn hex_color_to_u32<'de, D>(deserializer: D) -> Result<u32, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let hex_str: String = Deserialize::deserialize(deserializer)?;
+    // Remove leading `#` if present, then parse the hex string
+    u32::from_str_radix(hex_str.trim_start_matches('#'), 16).map_err(serde::de::Error::custom)
 }
 
